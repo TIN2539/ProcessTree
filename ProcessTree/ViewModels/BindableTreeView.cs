9ab42961102199d9330af57130f05090efdb344a
Алексей Бершаский <alesha60@hotmail.com>
2018-10-21 23:44:45 +0300

    Clean xaml

--- ProcessTree/ViewModels/BindableTreeView.cs
+++ ProcessTree/ViewModels/BindableTreeView.cs
@@ -8,25 +8,15 @@ namespace ProcessTree.ViewModels
         public static readonly DependencyProperty BindableSelectedItemProperty =
             DependencyProperty.Register("BindableSelectedItem", typeof(object), typeof(BindableTreeView), new PropertyMetadata(null));
 
-        public static readonly DependencyProperty IsSelectedSomeProcessProperty =
-            DependencyProperty.Register("IsSelectedSomeProcess", typeof(bool), typeof(BindableTreeView), new PropertyMetadata(null));
-
         public BindableTreeView()
         {
             SelectedItemChanged += (sender, e) => BindableSelectedItem = SelectedItem;
-            SelectedItemChanged += (sender, e) => IsSelectedSomeProcess = SelectedItem != null ? true : false;
         }
 
         public object BindableSelectedItem
         {
             get => GetValue(BindableSelectedItemProperty);
             set => SetValue(BindableSelectedItemProperty, value);
-        }
-
-        public bool IsSelectedSomeProcess
-        {
-            get { return (bool)GetValue(IsSelectedSomeProcessProperty); }
-            set { SetValue(IsSelectedSomeProcessProperty, value); }
         }
     }
 }
