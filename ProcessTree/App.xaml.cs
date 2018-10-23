84c88c6015bad037073b825a2dcefaee3f5c2414
Алексей Бершаский <alesha60@hotmail.com>
2018-10-24 01:34:53 +0300

    Use Ninject and Ninject.Extension

--- ProcessTree/App.xaml.cs
+++ ProcessTree/App.xaml.cs
@@ -1,19 +1,34 @@
-﻿using ProcessTree.ViewModels;
+﻿using Ninject;
+using Ninject.Extensions.Conventions;
+using ProcessTree.ViewModels;
 using System.Windows;
 
 namespace ProcessTree
 {
     public partial class App : Application
     {
+        private StandardKernel CreateContainer()
+        {
+            var container = new StandardKernel();
+
+            container.Bind(
+                configurator => configurator
+                .From("ProcessTree")
+                .SelectAllClasses()
+                .BindAllInterfaces()
+                );
+
+            return container;
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var treeManager = new ProcessTreeManager();
-            var viewModel = new MainViewModel(treeManager);
-            var view = new MainWindow(viewModel);
+            var container = CreateContainer();
+            MainWindow mainView = container.Get<MainWindow>();
 
-            view.Show();
+            mainView.Show();
         }
     }
 }
